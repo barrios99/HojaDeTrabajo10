51e5734e162214ecda2356c8eab5a7003374beff
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-05-16 05:02:30 -0600

    Add files via upload

--- HDT10.py
+++ HDT10.py
@@ -7,14 +7,34 @@
 #Documentacion extraida de https://neo4j-rest-client.readthedocs.io/en/latest/
 
 from neo4jrestclient.client import GraphDatabase
-gdb = GraphDatabase("http://localhost:7474/db/data/")
+gdb = GraphDatabase("http://localhost:7474", username="neo4j", password="12345")
 
 def addPaciente(nombre, genero, edad, peso estatura):
     paciente= gbd.nodes.create(Nombre:nombre, Genero:genero, Edad:edad, Peso:peso, Estatura:estatura)
     paciente.labels.add("Paciente")
 
-
 def addDoctor (nombre, especialidad, telefono, correo, ubicacion):
     doctor= gbd.nodes.create(Nombre:nombre, Especialidad:especialidad, Telefono:telefono, Correo:correo, Ubicacion:ubicacion)
     doctor.labels.add("Doctor")
+
+def addMedicina (nombre, dosis, vecesaldia, cantdias):
+    medicina = gbd.nodes.create(Nombre:nombre, Dosis:dosis, Veces:vecesaldia, Dias:cantdias)
+    medicina.labels.add("Medicina")
+
+def addVisita (nomdoctor, nompaciente):
+    pacientes= gbd.labels.get("Paciente")
+    pacientes.all()
+    doctores= gbd.labels.get("Doctor")
+    doctores.all()
+    pacientes.get(Nombre:nompaciente)[0].relationships.create("consulto",doctores.get(Nombre:nomdoctor)[0])
+
+def prescripcion (nompaciente, nommedicina, nomdoctor):
+    pacientes= gbd.labels.get("Paciente")
+    pacientes.all()
+    doctores= gbd.labels.get("Doctor")
+    doctores.all()
+    medicinas= gbd.labels.get("Medicina")
+    medicinas.all()
+    pacientes.get(Nombre:nompaciente)[0].relationships.create("toma",medicinas.get(Nombre:nommedicina)[0])
+    doctores.get(Nombre:nomdoctor)[0].relationships.create("receta",medicinas.get(Nombre:nommedicina)[0])
 
